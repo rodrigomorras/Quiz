6726f6a52313d59e9cfc21352113e19b2b20c3cf
rodrigomorras <rodrigo.mgutierrez@alumnos.upm.es>
2016-04-27 15:33:35 +0200

    Tema5: Marco de la aplicación

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
+
 
 var routes = require('./routes/index');
 
@@ -21,6 +23,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
+
+
 
 app.use('/', routes);
 
